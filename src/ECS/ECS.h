76c07d4127c5cf682412b01e2c23818a0bff1ca3
Alessio Micheloni <alessio.micheloni@gmail.com>
2023-02-17 18:22:58 +0100

    Implemented Pool class and Registry first draft

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -3,6 +3,8 @@
 
 #include <bitset>
 #include <vector>
+#include <unordered_map>
+#include <typeindex>
 
 const unsigned int MAX_COMPONENTS = 32;
 
@@ -72,9 +74,58 @@ class System
         template <typename TComponent> void RequireComponent();
 };
 
+/* POOL
+   A Pool is just a vector (contiguous data) of elements of type T.
+*/
+class IPool
+{
+    public:
+        virtual ~IPool() {}
+};
+
+template <typename T>
+class Pool : public IPool
+{
+    private:
+        std::vector<T> data;
+
+    public:
+        Pool(int size = 100) { Resize(size); }
+        virtual ~Pool() = default;
+
+        bool isEmpty() const { return data.empty(); }
+        int GetSize() const { return data.size(); }
+        void Resize(int newSize) { data.resize(newSize); }
+        void Clear() { data.clear(); }
+        void Add(T obj) { data.push_back(obj); }
+        void Set(int n, T obj) { data[n] = obj; }
+        T& Get(int n) const { return static_cast<T&>(data[n]); }
+
+        T& operator[](int n) const { return data[n]; }
+};
+
+/* REGISTRY
+   The registry manages the creation and destruction of entities and
+   is the responsible for adding systems and components.
+*/
 class Registry
 {
+    private:
+        int numEntities = 0;
 
+        // Each pool contains all the data for a certain component type
+        // Vector index = component type id, Pool index = entity id
+        std::vector<IPool*> componentPools;
+
+        // Component signatures (vector index = entity id)
+        std::vector<Signature> entityComponentSignature;
+
+        // Map of active systems (index = system type id)
+        std::unordered_map<std::type_index, System*> systems;
+
+    public:
+        Registry() = default;
+        ~Registry() = default;
 };
 
 template <typename TComponent>
